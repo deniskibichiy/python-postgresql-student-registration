e1ac85034b023e2b4aff48a49eebabb5dd6288b7
deniskibichiy <deniskibichiy@outlook.com>
2026-08-21 21:26:28 +0300

    establish a connection to the postgres db using psycopg

--- db_connection.py
+++ db_connection.py
@@ -1,9 +1,4 @@
 import psycopg
 
-conn = psycopg.connect(
-    dbname="sql_practice",
-    user="denis-kibichiy",   # or "postgres"
-    password="your_password",
-    host="localhost",
-    port=5432,
-)
+with psycopg.connect("host = localhost dbname = student user= denis password= Denis") as conn:
+    print("Connection to database established")
